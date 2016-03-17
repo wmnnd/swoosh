eb271c1eb1e07a10402600334962fb285576d7e2
Steve Domin <steve@stevedomin.com>
2016-03-17 01:07:35 +0000

    Add SMTP adapter
    This is the first version of an SMTP adapter backed by gen_stmp.
    It only supports text/plain, text/html, and multipart/alternative parts
    with limited support for configuring the Content-Type,
    Content-Transfer-Encoding of each individual parts.
    
    There is also no support for attachments since Swoosh doesn't support it
    yet.
    
    The testing is not as good as I would like it to be, ideally I would
    want to test whatever the mimemail modules returns (the string that will
    be sent to the server), rather than the tuple I'm generating.

--- test/swoosh/adapters/smtp_test.exs
+++ test/swoosh/adapters/smtp_test.exs
@@ -1,3 +1,95 @@
 defmodule Swoosh.Adapters.SMTPTest do
-  use ExUnit.Case, async: true
+  use AdapterCase, async: true
+
+  import Swoosh.Email
+  alias Swoosh.Adapters.SMTP
+
+  setup_all do
+    valid_email =
+    %Swoosh.Email{}
+    |> from("tony@stark.com")
+    |> to("steve@rogers.com")
+    |> subject("Hello, Avengers!")
+    |> html_body("<h1>Hello</h1>")
+    |> text_body("Hello")
+
+    {:ok, valid_email: valid_email}
+  end
+
+  test "simple email", %{valid_email: email} do
+    email = email |> html_body(nil)
+    assert SMTP.prepare_message(email) ==
+    {"text", "plain",
+      [{"Content-Type", "text/plain; charset=\"utf-8\""},
+        {"From", "tony@stark.com"},
+        {"To", "steve@rogers.com"},
+        {"Subject", "Hello, Avengers!"},
+        {"Mime-Version", "1.0"}],
+      "Hello"}
+  end
+
+  test "simple email with multiple recipients", %{valid_email: email} do
+    email = email |> html_body(nil) |> to({"Bruce Banner", "bruce@banner.com"})
+    assert SMTP.prepare_message(email) ==
+    {"text", "plain",
+      [{"Content-Type", "text/plain; charset=\"utf-8\""},
+        {"From", "tony@stark.com"},
+        {"To", "Bruce Banner <bruce@banner.com>, steve@rogers.com"},
+        {"Subject", "Hello, Avengers!"},
+        {"Mime-Version", "1.0"}],
+      "Hello"}
+  end
+
+  test "simple email with multiple cc recipients", %{valid_email: email} do
+    email =
+    email
+    |> html_body(nil)
+    |> to({"Bruce Banner", "bruce@banner.com"})
+    |> cc("thor@odinson.com")
+
+    assert SMTP.prepare_message(email) ==
+      {"text", "plain",
+       [{"Content-Type", "text/plain; charset=\"utf-8\""},
+        {"From", "tony@stark.com"},
+        {"To", "Bruce Banner <bruce@banner.com>, steve@rogers.com"},
+        {"Cc", "thor@odinson.com"},
+        {"Subject", "Hello, Avengers!"},
+        {"Mime-Version", "1.0"}],
+       "Hello"}
+  end
+
+  test "simple html email", %{valid_email: email} do
+    email = email |> text_body(nil)
+    assert SMTP.prepare_message(email) ==
+      {"text", "html",
+       [{"Content-Type", "text/html; charset=\"utf-8\""},
+        {"From", "tony@stark.com"},
+        {"To", "steve@rogers.com"},
+        {"Subject", "Hello, Avengers!"},
+        {"Mime-Version", "1.0"}],
+      "<h1>Hello</h1>"}
+  end
+
+  test "multipart/alternative email", %{valid_email: email} do
+    assert SMTP.prepare_message(email) ==
+      {"multipart", "alternative",
+       [{"From", "tony@stark.com"},
+        {"To", "steve@rogers.com"},
+        {"Subject", "Hello, Avengers!"},
+        {"Mime-Version", "1.0"}],
+       [{"text", "plain",
+         [{"Content-Type", "text/plain; charset=\"utf-8\""},
+          {"Content-Transfer-Encoding", "quoted-printable"}],
+         [{"content-type-params", [{"charset", "utf-8"}]},
+          {"disposition", "inline"},
+          {"disposition-params", []}],
+         "Hello"},
+        {"text", "html",
+         [{"Content-Type", "text/html; charset=\"utf-8\""},
+          {"Content-Transfer-Encoding", "quoted-printable"}],
+         [{"content-type-params", [{"charset", "utf-8"}]},
+          {"disposition", "inline"},
+          {"disposition-params", []}],
+        "<h1>Hello</h1>"}]}
+  end
 end
