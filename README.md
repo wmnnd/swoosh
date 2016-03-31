af30829687b026a58a644576421d1c181dc274a6
Steve Domin <steve@stevedomin.com>
2016-03-31 23:34:20 +0100

    Releave v0.2.0

--- README.md
+++ README.md
@@ -60,7 +60,7 @@ end
 
     ```elixir
     def deps do
-      [{:swoosh, "~> 0.1.0"}]
+      [{:swoosh, "~> 0.2.0"}]
     end
     ```
 
--- lib/swoosh.ex
+++ lib/swoosh.ex
@@ -1,7 +1,7 @@
 defmodule Swoosh do
   @moduledoc File.read!("README.md") |> String.replace("# Swoosh\n\n", "", global: false)
 
-  @version "0.1.0"
+  @version "0.2.0"
 
   @doc false
   def version, do: @version
