2935643bb8f26aeb10f5b0053cafa4b6f71bee15
Eric Lathrop <eric@ericlathrop.com>
2016-12-08 12:18:18 -0500

    Support SendGrid templates and substitutions (#111)

--- lib/swoosh/adapters/sendgrid.ex
+++ lib/swoosh/adapters/sendgrid.ex
@@ -49,6 +49,7 @@ defmodule Swoosh.Adapters.Sendgrid do
     |> prepare_subject(email)
     |> prepare_content(email)
     |> prepare_reply_to(email)
+    |> prepare_template_id(email)
   end
 
   defp email_item({"", email}), do: %{email: email}
@@ -63,6 +64,7 @@ defmodule Swoosh.Adapters.Sendgrid do
       |> prepare_cc(email)
       |> prepare_bcc(email)
       |> prepare_custom_vars(email)
+      |> prepare_substitutions(email)
 
     Map.put(body, :personalizations, [personalizations])
   end
@@ -83,6 +85,11 @@ defmodule Swoosh.Adapters.Sendgrid do
   end   
   defp prepare_custom_vars(personalizations, _email), do: personalizations
 
+  defp prepare_substitutions(personalizations, %Email{provider_options: %{substitutions: substitutions}}) do
+    Map.put(personalizations, :substitutions, substitutions)
+  end
+  defp prepare_substitutions(personalizations, _email), do: personalizations
+
   defp prepare_subject(body, %Email{subject: subject}), do: Map.put(body, :subject, subject)
 
   defp prepare_content(body, %Email{html_body: html, text_body: text}) do
@@ -98,4 +105,9 @@ defmodule Swoosh.Adapters.Sendgrid do
 
   defp prepare_reply_to(body, %Email{reply_to: nil}), do: body
   defp prepare_reply_to(body, %Email{reply_to: reply_to}), do: Map.put(body, :reply_to, reply_to |> email_item)
+
+  defp prepare_template_id(body, %Email{provider_options: %{template_id: template_id}}) do
+    Map.put(body, :template_id, template_id)
+  end
+  defp prepare_template_id(body, _email), do: body
 end
