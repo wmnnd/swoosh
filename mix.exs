f7cdb220d20f5c46f319098d54b184275a3300ed
Steve Domin <steve@stevedomin.com>
2017-04-24 15:45:39 +0300

    Bump bypass to 0.6

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule Swoosh.Mixfile do
      {:cowboy, "~> 1.0", optional: true},
      {:plug, "~> 1.1", optional: true},
      {:credo, "~> 0.7", only: [:dev, :test]},
-     {:bypass, "~> 0.5", only: :test},
+     {:bypass, "~> 0.6", only: :test},
      {:ex_doc, "~> 0.15", only: :docs},
      {:inch_ex, ">= 0.0.0", only: :docs}]
   end
